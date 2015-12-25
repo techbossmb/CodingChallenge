ed461032f434d710e84e1844ea71f5129f0f04f9
Ishola Babatunde Isaac <tecbossmb@gmail.com>
2015-12-24 23:44:03 -0600

    WordGrid problem
    Given n by n grid of alphabets and a dictionary, find all possible words that can be formed by traversing through the grid in all 8 direction

---
+++ WordGrid.java
@@ -0,0 +1,138 @@
+import java.util.*;
+import java.io.*;
+/*
+ * WordGrid problem
+ * Given n by n grid of alphabets and a dictionary, 
+ * find all possible words that can be formed by traversing through the grid in all 8 direction
+ * @author: Ishola Babatunde
+ * @date: 12/24/2015
+ * @todo: Need to implement dictionary as a Trie to provide isPrefix() function for backtracking
+ *        Build graph appropriately (automate) - get rid of manual buildNodes()
+ * */
+public class WordGrid{
+	
+    static ArrayList<Node> nodes = new ArrayList<>();
+    static ArrayList<String> foundWords = new ArrayList<>();
+    static Set<String> dictionary = new HashSet<>(); //for O(1) complexity on .contains()
+    
+    public static void main(String[] args){
+		WordGrid wordGrid = new WordGrid();
+		nodes = wordGrid.buildNodes();
+        String[] newWords = {"abc", "bcei", "cedi", "edg", "fehi", "ghi"};
+        dictionary = wordGrid.buildDictionary(dictionary, newWords);
+        //use all nodes as starting node
+        for(int i = 0; i < nodes.size(); i++){
+			Node _curNode = nodes.get(i);
+			wordGrid.searchWordRecursive(_curNode.value, _curNode);
+			wordGrid.unmarkNodes(nodes);
+		}
+		System.out.println(foundWords);
+	}
+	
+	/*DFS search for a words contained in the dictionary that appear on the grid
+	 * max word possible is 9 for a 3 x 3 grid if no duplication
+	 * @param: current string, and current 'root' node
+	 * */
+	private void searchWordRecursive(String curStr, Node curNode){
+		curNode.isVisited = true;
+		if(dictionary.contains(curStr)){
+			foundWords.add(curStr);
+		}
+		
+		ArrayList<Node> neighbours = curNode.getNeighbours();
+		for(int i = 0; i < neighbours.size(); i++){
+			Node curNeighbour = neighbours.get(i);
+			if(!curNeighbour.isVisited){
+				String newStr = curStr.concat(curNeighbour.value);
+				if(newStr.length() <=  9)
+					searchWordRecursive(newStr, curNeighbour);
+			}
+		}
+		curNode.isVisited = false;
+	}
+	
+	/*clear all nodes once done
+	 * */
+	private void unmarkNodes(ArrayList<Node> nodes){
+		for(Node node:nodes){
+			node.isVisited = false;
+		}
+	}
+    
+    /* populates dictionary
+     * @param: dictionary set, array of new words to add
+     * @returnValue: populated dictionary
+    */
+    private Set<String> buildDictionary(Set<String> dictionary, String[] words){
+        for(int i = 0; i < words.length; i++){
+            dictionary.add(words[i]);
+        }
+        return dictionary;
+    }
+    
+    /* manually build nodes, a 3 x 3 grid with alphabets a to i, with their connections
+     * @returnValue: list of nodes (each node with its own neighbours)
+    */
+    private ArrayList<Node> buildNodes(){
+		ArrayList<Node> nodes = new ArrayList<>();
+        Node nodeA = new Node("a");
+        Node nodeB = new Node("b");
+        Node nodeC = new Node("c");
+        Node nodeD = new Node("d");
+        Node nodeE = new Node("e");
+        Node nodeF = new Node("f");
+        Node nodeG = new Node("g");
+        Node nodeH = new Node("h");
+        Node nodeI = new Node("i");
+        nodeA.addNeighbours(new Node[]{nodeB, nodeE, nodeD});
+        nodeB.addNeighbours(new Node[]{nodeC, nodeA, nodeD, nodeE, nodeF});
+        nodeC.addNeighbours(new Node[]{nodeB, nodeE, nodeF});
+        nodeD.addNeighbours(new Node[]{nodeG, nodeA, nodeB, nodeE, nodeH});
+        nodeE.addNeighbours(new Node[]{nodeI, nodeH, nodeE, nodeA, nodeB, nodeC, nodeD, nodeF, nodeG});
+        nodeF.addNeighbours(new Node[]{nodeC, nodeB, nodeE, nodeH, nodeI});
+        nodeG.addNeighbours(new Node[]{nodeD, nodeH, nodeC});
+        nodeH.addNeighbours(new Node[]{nodeI, nodeG, nodeD, nodeE, nodeF});
+        nodeI.addNeighbours(new Node[]{nodeF, nodeE, nodeH});
+       
+        nodes.add(nodeA);
+        nodes.add(nodeB);
+        nodes.add(nodeC);
+        nodes.add(nodeD);
+        nodes.add(nodeE);
+        nodes.add(nodeF);
+        nodes.add(nodeG);
+        nodes.add(nodeH);
+        nodes.add(nodeI);
+        return nodes;
+    }
+
+}
+
+/*
+ * Seems unnecessary - could have achieved the same thing with just indexing
+ * Need to analyse the complexity of using a Node class
+ * */
+class Node{
+    public String value = null;
+    public ArrayList<Node> neighbours = null;
+    public Boolean isVisited = false;
+    
+    public Node(String value){
+        this.value = value;
+        this.neighbours = new ArrayList<>();
+    }
+    
+    public void addNeighbours(Node[] neighbours){
+        for (int i = 0; i < neighbours.length; i++){
+            this.neighbours.add(neighbours[i]);
+        }
+    }
+    
+    public ArrayList<Node> getNeighbours(){
+        return this.neighbours;
+    }
+    
+    public String toString(){
+		return this.value;
+	}
+}
